2cfc8f7b2ed1e7287868eec7524d2d1b8f35b142
benjilollebg <benjamin.demartin@hotmail.fr>
2023-02-10 12:43:55 +0000

    propre

--- NIC/dma/host_memory/dma.c
+++ NIC/dma/host_memory/dma.c
@@ -31,6 +31,7 @@
 #include <utils.h>
 
 #include "dma_common.h"
+#include <../../../utils/dma_exchange.h>
 
 DOCA_LOG_REGISTER(DMA_WRITE_DPU);
 
@@ -40,86 +41,8 @@ DOCA_LOG_REGISTER(DMA_WRITE_DPU);
 
 #define IP "192.168.100.1"
 #define PORT 6666
-#define PCIE_ADDR "03:00:0"		/* Setting the pcie addr (you should modify it) */
+#define PCIE_ADDR "03:00.0"		/* Setting the pcie addr (you should modify it) */
 
-/*
- * Saves export descriptor and buffer information content into memory buffers
- *
- * @export_desc_file_path [in]: Export descriptor file path
- * @buffer_info_file_path [in]: Buffer information file path
- * @export_desc [in]: Export descriptor buffer
- * @export_desc_len [in]: Export descriptor buffer length
- * @remote_addr [in]: Remote buffer address
- * @remote_addr_len [in]: Remote buffer total length
- * @return: DOCA_SUCCESS on success and DOCA_ERROR otherwise
- */
-
-doca_error_t
-receive_data_from_host(char *export_desc, size_t *export_desc_len, char **remote_addr, size_t *remote_addr_len)
-{
-	int sock_fd;
-	int result;
-	char buffer[RECV_BUF_SIZE];
-
-	struct sockaddr_in servaddr, client;
-
-    	sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
-    	if (sock_fd == -1) {
-        	DOCA_LOG_ERR("socket creation failed...");
-        	return DOCA_ERROR_IO_FAILED;
-    	}
-
-	//servaddr.sin_addr.s_addr = inet_addr("192.168.100.1");
-        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-        servaddr.sin_family = AF_INET;
-        servaddr.sin_port = htons(PORT);
-
-	result = bind(sock_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-	if (result != 0){
-        	DOCA_LOG_INFO("Socket bind failed...");
-        	return DOCA_ERROR_IO_FAILED;
-    	}
-
-	DOCA_LOG_ERR("Receiving data");
-
-	/* Receive the descriptor on the socket */
-	socklen_t client_len = sizeof(client);
-	*export_desc_len = recvfrom(sock_fd, export_desc, 1024, 0, (struct sockaddr *) &client, &client_len);
-    	if (*export_desc_len < 0) {
-        	DOCA_LOG_ERR("Couldn't receive data from host");
-		close(sock_fd);
-                return DOCA_ERROR_IO_FAILED;
-    	}
-	DOCA_LOG_INFO("export_desc : %s", export_desc);
-        DOCA_LOG_INFO("export_desc_len : %ld", *export_desc_len);
-//	write(1, export_desc, *export_desc_len);
-
-	/* Receive the buffer address on the socket */
-	int bytes_received = recvfrom(sock_fd, buffer, RECV_BUF_SIZE, 0, (struct sockaddr *) &client, &client_len);
-        if (bytes_received < 0) {
-                DOCA_LOG_ERR("Couldn't receive data from host");
-		close(sock_fd);
-                return DOCA_ERROR_IO_FAILED;
-        }
-	*remote_addr = (char*) strtoull(buffer, NULL, 0);
-	DOCA_LOG_INFO("remote_addr : %lld", strtoull(buffer, NULL, 0));
-
-        memset(buffer, 0, RECV_BUF_SIZE);
-
-	/* Receive the buffer length on the socket */
-	bytes_received = recvfrom(sock_fd, buffer, RECV_BUF_SIZE, 0, (struct sockaddr *) &client, &client_len);
-        if (bytes_received < 0) {
-                DOCA_LOG_ERR("Couldn't receive data from host");
-		close(sock_fd);
-                return DOCA_ERROR_IO_FAILED;
-        }
-	*remote_addr_len = strtoull(buffer, NULL, 0);
-	DOCA_LOG_INFO("remote_addr_len : %ld", *remote_addr_len);
-
-	DOCA_LOG_INFO("Exported data was received");
-
-	return DOCA_SUCCESS;
-}
 
 doca_error_t
 read_write(struct doca_dma_job_memcpy dma_job, struct program_core_objects state, struct timespec ts, struct doca_event event)
@@ -203,7 +126,7 @@ dma_write(struct doca_pci_bdf *pcie_addr)
         }
 
 	/* Receive exported data from host */
-        result = receive_data_from_host(export_desc, &export_desc_len, &remote_addr, &remote_addr_len);
+        result = receive_dma_data(export_desc, &export_desc_len, &remote_addr, &remote_addr_len, PORT);
         if (result != DOCA_SUCCESS) {
                 dma_cleanup(&state, dma_ctx);
                 return DOCA_ERROR_NOT_CONNECTED;
